349b85da2a921123b77ecfa28b1c948c575cb907
David Novaković <daki.nole@gmail.com>
2026-05-09 15:38:21 +0200

    Added Candidate model

---
+++ Server/Server/Domain/Models/Candidate.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Domain.Models
+{
+    public class Candidate
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; } = DateOnly.MaxValue;
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
