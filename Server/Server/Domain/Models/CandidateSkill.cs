1340d06e62d06413763690e6f939ea0053801de5
David Novaković <daki.nole@gmail.com>
2026-05-09 15:38:56 +0200

    Added CandidateSkill model (table that connects Skill and Candidate)

---
+++ Server/Server/Domain/Models/CandidateSkill.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Domain.Models
+{
+    public class CandidateSkil
+    {
+        public string SkillId { get; set; } = string.Empty;
+        public string CandidateId { get; set; } = string.Empty;
+    }
+}
