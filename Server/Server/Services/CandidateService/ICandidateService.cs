f458113f072ec21a5a376b78a82b87c481870657
David Novaković <daki.nole@gmail.com>
2026-05-10 01:45:18 +0200

    Improved return types from ICandidateService definition

--- Server/Server/Services/CandidateService/ICandidateService.cs
+++ Server/Server/Services/CandidateService/ICandidateService.cs
@@ -5,11 +5,11 @@ namespace Server.Services.CandidateService
 {
     public interface ICandidateService
     {
-        // TODO: improve candidate service interface definition Task types and parameters
+        // TODO: add Result pattern return types for all methods
         public Task<Candidate> AddCandidateAsync(CreateCandidateDto dto);
-        public Task RemoveCandidateAsync(int id);
+        public Task<bool> RemoveCandidateAsync(int id);
         public Task<List<Candidate>> GetCandidateAsync(string? name, List<int>? skillIds);
-        public Task AddSkillToCandidateAsync(int candidateId, int skillId);
-        public Task RemoveSkillFromCandidateAsync(int candidateId, int skillId);
+        public Task<bool> AddSkillToCandidateAsync(int candidateId, int skillId);
+        public Task<bool> RemoveSkillFromCandidateAsync(int candidateId, int skillId);
     }
 }
