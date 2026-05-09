ea915174bfc1f7bd038fe081b4ee88f1e1b25806
David Novaković <daki.nole@gmail.com>
2026-05-10 01:16:17 +0200

    Improved parameters in ICandidateService interface definition

--- Server/Server/Services/CandidateService/ICandidateService.cs
+++ Server/Server/Services/CandidateService/ICandidateService.cs
@@ -1,13 +1,14 @@
-﻿using Server.Domain.Models;
+﻿using Server.Domain.DTOs;
+using Server.Domain.Models;
 
 namespace Server.Services.CandidateService
 {
     public interface ICandidateService
     {
         // TODO: improve candidate service interface definition Task types and parameters
-        public Task<Candidate> AddCandidateAsync(/* TODO: add DTO here */);
+        public Task<Candidate> AddCandidateAsync(CreateCandidateDto dto);
         public Task RemoveCandidateAsync(int id);
-        public Task<Candidate> GetCandidateAsync(/* TODO: add DTO here */);
+        public Task<List<Candidate>> GetCandidateAsync(string? name, List<int>? skillIds);
         public Task AddSkillToCandidateAsync(int candidateId, int skillId);
         public Task RemoveSkillFromCandidateAsync(int candidateId, int skillId);
     }
