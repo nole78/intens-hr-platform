edcf3c5cda4f8749e9a2a7ef481406a69e3a5420
David Novaković <daki.nole@gmail.com>
2026-05-10 00:43:00 +0200

    Added base interface definition for CandidateService

--- Server/Server/Services/CandidateService/ICandidateService.cs
+++ Server/Server/Services/CandidateService/ICandidateService.cs
@@ -1,7 +1,14 @@
-﻿namespace Server.Services.CandidateService
+﻿using Server.Domain.Models;
+
+namespace Server.Services.CandidateService
 {
     public interface ICandidateService
     {
-        // TODO: define candidate service interface
+        // TODO: improve candidate service interface definition Task types and parameters
+        public Task<Candidate> AddCandidateAsync(/* TODO: add DTO here */);
+        public Task RemoveCandidateAsync(int id);
+        public Task<Candidate> GetCandidateAsync(/* TODO: add DTO here */);
+        public Task AddSkillToCandidateAsync(int candidateId, int skillId);
+        public Task RemoveSkillFromCandidateAsync(int candidateId, int skillId);
     }
 }
