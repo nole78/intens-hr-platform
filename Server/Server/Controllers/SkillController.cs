c006c0022c138859e9b8e4ff54b77da82241e23b
David Novaković <daki.nole@gmail.com>
2026-05-09 15:29:40 +0200

    Added Skill Controller route (not implemented)

---
+++ Server/Server/Controllers/SkillController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SkillController : ControllerBase
+    {
+        // TODO: add services for skill management
+        public SkillController()
+        {
+            // TODO: add dependency injection for services
+        }
+
+        [HttpPost]
+        public ActionResult<string> AddSkill()
+        {
+            // TODO: implement
+            return StatusCode(500, new { message = "Not implemented yet" });
+        }
+    }
+}
