ee42230cc8154985fad83781d501ba54e2110136
David Novaković <daki.nole@gmail.com>
2026-05-09 23:45:35 +0200

    Defined base methods for ISkillRepository

--- Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
+++ Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Server.Persistance.Repositories.Skills
+﻿using Server.Domain.Models;
+
+namespace Server.Persistance.Repositories.Skills
 {
     public interface ISkillRepository
     {
         // TODO: define methods for managing skills in the database
+        public Task<bool> AddSkill(Skill skill);
+        public Task<Skill> GetById(string id);
+        public Task<Skill> GetByName(string name);
     }
 }
