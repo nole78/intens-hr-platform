611e1fa6948a692f4af6e1d0e64d4ff53c76dd36
David Novaković <daki.nole@gmail.com>
2026-05-10 01:45:40 +0200

    Implemented CandidateController with canidate service DI

--- Server/Server/Controllers/CandidateController.cs
+++ Server/Server/Controllers/CandidateController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Server.Domain.DTOs;
+using Server.Domain.Models;
+using Server.Services.CandidateService;
+using System.Threading.Tasks;
 
 
 namespace Server.Controllers
@@ -8,38 +12,52 @@ namespace Server.Controllers
     [Route("api/[controller]")]
     public class CandidateController : ControllerBase
     {
-        // TODO: add services for candidate management and skill management
-        public CandidateController()
+        // TODO: add validation services
+        private readonly ICandidateService _candidateService;
+        public CandidateController(ICandidateService candidateService)
         {
-            // TODO: add dependency injection for services
+            // TODO: add dependency injection for validation services
+            _candidateService = candidateService;
         }
 
         [HttpPost]
-        public ActionResult<string> AddCandidate()
+        public async Task<ActionResult<string>> AddCandidate([FromBody] CreateCandidateDto dto)
         {
-            // TODO: implement
-            return StatusCode(500, new { message = "Not implemented yet" });
+            // TODO: improve with Result pattern return type and error handling
+            var result = await _candidateService.AddCandidateAsync(dto);
+            if(result == null)
+            {
+                return BadRequest(new { message = "Couldn't create candidate" });
+            }
+            return Ok(result);
         }
 
-        [HttpPatch("add-skill")]
-        public ActionResult<string> AddSkillToCandidate()
+        [HttpPatch]
+        public async Task<ActionResult<string>> AddSkillToCandidate([FromQuery] int candidateId,[FromQuery] int skillId)
         {
-            // TODO: implement
-            return StatusCode(500, new { message = "Not implemented yet" });
+            // TODO: improve with Result pattern return type and error handling
+            var result = await _candidateService.AddSkillToCandidateAsync(candidateId, skillId);
+            return Ok(result);
         }
 
-        [HttpPatch("remove-skill")]
-        public ActionResult<string> RemoveSkillFromCandidate()
+        [HttpDelete]
+        public ActionResult<string> RemoveSkillFromCandidate([FromQuery]int id)
         {
-            // TODO: implement
-            return StatusCode(500, new { message = "Not implemented yet" });
+            // TODO: improve with Result pattern return type and error handling
+            var result = _candidateService.RemoveCandidateAsync(id);
+            return result.Result ? Ok(new { message = "Candidate removed successfully" }) : BadRequest(new { message = "Couldn't remove candidate" });
         }
 
         [HttpGet]
-        public ActionResult<string> GetCandidate()
+        public async Task<ActionResult<List<Candidate>>> Search([FromQuery] string? name,[FromQuery] List<int>? skillIds)
         {
-            // TODO: implement
-            return StatusCode(500, new { message = "Not implemented yet" });
+            // TODO: improve with Result pattern return type and error handling
+            if (string.IsNullOrWhiteSpace(name) && (skillIds == null || skillIds.Count == 0))
+            {
+                return BadRequest(new { message = "At least one of the parameters must be provided" });
+            }
+            var result = await _candidateService.GetCandidateAsync(name, skillIds);
+            return Ok(result);        
         }
     }
 }
