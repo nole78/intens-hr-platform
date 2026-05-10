5250cec7c000a97a8531051e0207caecf93e9156
David Novaković <daki.nole@gmail.com>
2026-05-11 01:13:34 +0200

    Added candidate skill repository interface and implementation

---
+++ Server/Server/Persistance/Repositories/CandidateSkills/CandidateSkillRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Domain.Models;
+using Server.Persistance.Context;
+
+namespace Server.Persistance.Repositories.CandidateSkills
+{
+    public class CandidateSkillRepository : ICandidateSkillRepository
+    {
+        private readonly AppDbContext _context;
+        public CandidateSkillRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> AddCandidateSkill(int candidateId, int skillId)
+        {
+            var exists = await _context.CandidateSkills.AnyAsync(cs => cs.CandidateId == candidateId && cs.SkillId == skillId);
+
+            if(exists)
+                return false;
+
+            _context.CandidateSkills.Add(new CandidateSkill
+            {
+                CandidateId = candidateId,
+                SkillId = skillId
+            });
+
+            var changes = await _context.SaveChangesAsync();
+            return changes > 0;
+        }
+
+        public async Task<bool> DeleteCandidateSkill(int candidateId, int skillId)
+        {
+            var candidateSkill = _context.CandidateSkills.FirstOrDefault(cs => cs.CandidateId == candidateId && cs.SkillId == skillId);
+        
+            if(candidateSkill == null)
+                return false;
+
+            _context.CandidateSkills.Remove(candidateSkill);
+
+            var changes = await _context.SaveChangesAsync();
+            return changes > 0;
+        }
+    }
+}
---
+++ Server/Server/Persistance/Repositories/CandidateSkills/ICandidateSkillRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Persistance.Repositories.CandidateSkills
+{
+    public interface ICandidateSkillRepository
+    {
+        Task<bool> AddCandidateSkill(int candidateId, int skillId);
+        Task<bool> DeleteCandidateSkill(int candidateId, int skillId);
+    }
+}
