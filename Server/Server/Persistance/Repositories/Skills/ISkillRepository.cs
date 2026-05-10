c3621272855ee32eec54885878b879bc3f5573b1
David Novaković <daki.nole@gmail.com>
2026-05-11 01:29:19 +0200

    Added ExistsAsync method to skill repo and improved naming to match conventions

--- Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
+++ Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
@@ -5,8 +5,9 @@ namespace Server.Persistance.Repositories.Skills
     public interface ISkillRepository
     {
         // TODO: define methods for managing skills in the database
-        public Task<Skill> AddSkill(Skill skill);
-        public Task<Skill?> GetById(int id);
-        public Task<Skill?> GetByName(string name);
+        public Task<Skill> AddSkillAsync(Skill skill);
+        public Task<Skill?> GetByIdAsync(int id);
+        public Task<Skill?> GetByNameAsync(string name);
+        public Task<bool> ExistsAsync(int id);
     }
 }
--- Server/Server/Persistance/Repositories/Skills/SkillRepository.cs
+++ Server/Server/Persistance/Repositories/Skills/SkillRepository.cs
@@ -7,24 +7,28 @@ namespace Server.Persistance.Repositories.Skills
     public class SkillRepository : ISkillRepository
     {
         private readonly AppDbContext _context;
-        // TODO: Implement methods for managing skills in the database
         public SkillRepository(AppDbContext context)
         {
             _context = context;
         }
-        public async Task<Skill> AddSkill(Skill skill)
+        public async Task<Skill> AddSkillAsync(Skill skill)
         {
             var added = await _context.Skills.AddAsync(skill);
             await _context.SaveChangesAsync();
             return added.Entity;
         }
 
-        public  Task<Skill?> GetById(int id)
+        public Task<bool> ExistsAsync(int id)
+        {
+            return _context.Skills.AnyAsync(s => s.Id == id);
+        }
+
+        public  Task<Skill?> GetByIdAsync(int id)
         {
             return _context.Skills.FirstOrDefaultAsync(s => s.Id == id);
         }
 
-        public async Task<Skill?> GetByName(string name)
+        public async Task<Skill?> GetByNameAsync(string name)
         {
             var normalizedName = name.Trim().ToLower();
             return await _context.Skills.FirstOrDefaultAsync(s => s.Name.ToLower() == normalizedName);
