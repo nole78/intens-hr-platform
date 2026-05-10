95a6be28d0d53d9588c8ed40e2311273e919635a
David Novaković <daki.nole@gmail.com>
2026-05-11 01:39:33 +0200

    Implemented CandidateService

--- Server/Server/Services/CandidateService/CandidateService.cs
+++ Server/Server/Services/CandidateService/CandidateService.cs
@@ -1,6 +1,8 @@
 ﻿using Server.Domain.DTOs;
 using Server.Domain.Models;
 using Server.Persistance.Repositories.Candidates;
+using Server.Persistance.Repositories.CandidateSkills;
+using Server.Persistance.Repositories.Skills;
 
 namespace Server.Services.CandidateService
 {
@@ -8,33 +10,86 @@ namespace Server.Services.CandidateService
     {
         // TODO: implement candidate service
         private readonly ICandidateRepository _candidateRepository;
-        public CandidateService(ICandidateRepository candidateRepository)
+        private readonly ICandidateSkillRepository _candidateSkillRepository;
+        private readonly ISkillRepository _skillRepository;
+        public CandidateService(ICandidateRepository candidateRepository,ISkillRepository skillRepository,ICandidateSkillRepository candidateSkillRepository)
         {
             _candidateRepository = candidateRepository;
+            _candidateSkillRepository = candidateSkillRepository;
+            _skillRepository = skillRepository;
         }
-        public Task<Candidate> AddCandidateAsync(CreateCandidateDto dto)
+        public async Task<Candidate> AddCandidateAsync(CreateCandidateDto dto)
         {
-            throw new NotImplementedException();
+            // TODO: implement result pattern
+            var exists = await _candidateRepository.GetByEmailAsync(dto.Email);
+            if (exists != null)
+            {
+                return new Candidate();
+            }
+            return await _candidateRepository.AddAsync(new Candidate
+            {
+                Name = dto.Name,
+                ContactNumber = dto.ContactNumber,
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+            });
         }
 
-        public Task<bool> AddSkillToCandidateAsync(int candidateId, int skillId)
+        public async Task<bool> AddSkillToCandidateAsync(int candidateId, int skillId)
         {
-            throw new NotImplementedException();
+            // TODO: implement result pattern
+            var candidateExists = await _candidateRepository.ExistsAsync(candidateId);
+            if (!candidateExists) { 
+                return false; 
+            }
+            var skillExists = await _skillRepository.ExistsAsync(skillId);
+            if (!skillExists) {
+                return false;
+            }
+            var connectionExists = await _candidateSkillRepository.ExistsAsync(candidateId, skillId);
+            if (connectionExists) {
+                return false;
+            }
+
+            return await _candidateSkillRepository.AddCandidateSkillAsync(candidateId,skillId);
         }
 
         public Task<List<Candidate>> GetCandidateAsync(string? name, List<int>? skillIds)
         {
-            throw new NotImplementedException();
+            // TODO: implement result pattern
+            return _candidateRepository.SearchAsync(name, skillIds);
         }
 
-        public Task<bool> RemoveCandidateAsync(int id)
+        public async Task<bool> RemoveCandidateAsync(int id)
         {
-            throw new NotImplementedException();
+            // TODO: implement result pattern
+            var exists = await _candidateRepository.ExistsAsync(id);
+            if (!exists)
+                return false;
+
+            var candidate = await _candidateRepository.GetByIdAsync(id);
+            if (candidate == null)
+                return false;
+
+            return await _candidateRepository.DeleteAsync(candidate);
         }
 
-        public Task<bool> RemoveSkillFromCandidateAsync(int candidateId, int skillId)
+        public async Task<bool> RemoveSkillFromCandidateAsync(int candidateId, int skillId)
         {
-            throw new NotImplementedException();
+            // TODO: implement result pattern
+            var candidateExists = await _candidateRepository.ExistsAsync(candidateId);
+            if (!candidateExists)
+                return false;
+
+            var skillExists = await _skillRepository.ExistsAsync(skillId);
+            if (!skillExists)
+                return false;
+
+            var connectionExists = await _candidateSkillRepository.ExistsAsync(candidateId, skillId);
+            if (!connectionExists)
+                return false;
+
+            return await _candidateSkillRepository.DeleteCandidateSkillAsync(candidateId, skillId);
         }
     }
 }
