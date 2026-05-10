4f269ba0967e461a531fbc8dd15c5b5dfb844363
David Novaković <daki.nole@gmail.com>
2026-05-10 02:35:44 +0200

    fixed skill service to take int id instead of string as parameter of GetById

--- Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
+++ Server/Server/Persistance/Repositories/Skills/ISkillRepository.cs
@@ -6,7 +6,7 @@ namespace Server.Persistance.Repositories.Skills
     {
         // TODO: define methods for managing skills in the database
         public Task<Skill> AddSkill(Skill skill);
-        public Task<Skill> GetById(string id);
+        public Task<Skill> GetById(int id);
         public Task<Skill> GetByName(string name);
     }
 }
