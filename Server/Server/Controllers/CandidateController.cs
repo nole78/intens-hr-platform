340cdb2269d0b9132e01f021f6b4d889393105c1
David Novaković <daki.nole@gmail.com>
2026-05-11 17:49:23 +0200

    Improved controllers with action result extensions helper class ToActionResult calls

--- Server/Server/Controllers/CandidateController.cs
+++ Server/Server/Controllers/CandidateController.cs
@@ -11,11 +11,9 @@ namespace Server.Controllers
     [Route("api/[controller]")]
     public class CandidateController : ControllerBase
     {
-        // TODO: add validation services
         private readonly ICandidateService _candidateService;
         public CandidateController(ICandidateService candidateService)
         {
-            // TODO: add dependency injection for validation services
             _candidateService = candidateService;
         }
 
@@ -23,76 +21,28 @@ namespace Server.Controllers
         public async Task<ActionResult<Candidate>> AddCandidate(CreateCandidateDto dto)
         {
             var result = await _candidateService.AddCandidateAsync(dto);
-            if(result.IsSucces)
-            {
-                return Ok(result.Value);
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = result.Error })
-                };
-            }
+            return ResultExstensions.ToActionResult(result);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> RemoveCandidate(int id)
         {
             var result = await _candidateService.RemoveCandidateAsync(id);
-            if(result.IsSucces)
-            {
-                return Ok();
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = result.Error })
-                };
-            }
+            return ResultExstensions.ToActionResult(result);
         }
 
         [HttpPost("{candidateId}/skills/{skillId}")]
         public async Task<ActionResult> AddSkillToCandidate(int candidateId,int skillId)
         {
             var result = await _candidateService.AddSkillToCandidateAsync(candidateId, skillId);
-            if (result.IsSucces)
-            {
-                return Ok();
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = result.Error })
-                };
-            }
+            return ResultExstensions.ToActionResult(result);
         }
 
         [HttpDelete("{candidateId}/skills/{skillId}")]
         public async Task<ActionResult> RemoveSkillFromCandidate(int candidateId, int skillId)
         {
             var result = await _candidateService.RemoveSkillFromCandidateAsync(candidateId, skillId);
-            if (result.IsSucces)
-            {
-                return Ok();
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = result.Error })
-                };
-            }
+            return ResultExstensions.ToActionResult(result);
         }
 
         [HttpGet]
@@ -103,20 +53,7 @@ namespace Server.Controllers
                 return BadRequest(new { message = "At least one of the parameters must be provided" });
             }
             var result = await _candidateService.GetCandidateAsync(name, skillIds);
-            if (result.IsSucces)
-            {
-                return Ok(result.Value);
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = result.Error })
-                };
-            }
-            ;        
+            return ResultExstensions.ToActionResult(result);
         }
     }
 }
--- Server/Server/Controllers/SkillController.cs
+++ Server/Server/Controllers/SkillController.cs
@@ -10,11 +10,9 @@ namespace Server.Controllers
     [Route("api/[controller]")]
     public class SkillController : ControllerBase
     {
-        // TODO: add validation services
         private readonly ISkillService _skillService;
         public SkillController(ISkillService skillService)
         {
-            // TODO: add dependency injection for validation services
             _skillService = skillService;
         }
 
@@ -22,19 +20,7 @@ namespace Server.Controllers
         public async Task<ActionResult<Skill>> AddSkill([FromBody] CreateSkillDto dto)
         {
             var result = await _skillService.AddSkillAsync(dto);
-            if(result.IsSucces)
-            {
-                return Ok(result.Value);
-            }
-            else
-            {
-                return result.ErrorType switch
-                {
-                    ErrorType.NotFound => NotFound(new { message = result.Error }),
-                    ErrorType.Validation => BadRequest(new { message = result.Error }),
-                    _ => StatusCode(500, new { message = "An error occurred while creating the skill" })
-                };
-            }
+            return ResultExstensions.ToActionResult(result);
         }
     }
 }
