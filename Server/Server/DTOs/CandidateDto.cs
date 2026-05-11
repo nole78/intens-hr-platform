a2189871e08d7c704fa9ee14a418acca392e9cb9
David Novaković <daki.nole@gmail.com>
2026-05-11 17:37:40 +0200

    Expanded CandidateDto with list of SkillDto objects for skills

---
+++ Server/Server/DTOs/CandidateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.DTOs
+{
+    public class CandidateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        public string ContactNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public List<SkillDto> Skills { get; set; } = [];
+    }
+}
