29797d56baa51329c7127e7eb689467937d9d386
David Novaković <daki.nole@gmail.com>
2026-05-11 17:47:33 +0200

    Added ResultEsxtension class for cleaner Controller

---
+++ Server/Server/Common/ResultExstensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Common
+{
+    public static class ResultExstensions
+    {
+        public static ActionResult ToActionResult(Result result)
+        {
+            if(result.IsSuccess)
+            {
+                return new NoContentResult();
+            }
+
+            return result.ErrorType switch
+            {
+                ErrorType.Validation => new BadRequestObjectResult(new { message = result.Error }),
+                ErrorType.NotFound => new NotFoundObjectResult(new { message = result.Error }),
+                _ => new ObjectResult(new { message = result.Error }) { StatusCode = 500 }
+            };
+        }
+
+        public static ActionResult ToActionResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                return new OkObjectResult(result.Value);
+            }
+
+            return result.ErrorType switch
+            {
+                ErrorType.Validation => new BadRequestObjectResult(new { message = result.Error }),
+                ErrorType.NotFound => new NotFoundObjectResult(new { message = result.Error }),
+                _ => new ObjectResult(new { message = result.Error }) { StatusCode = 500 }
+            };
+        }
+    }
+}
