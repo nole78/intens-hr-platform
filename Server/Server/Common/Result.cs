8c309e9f8e6d5155ce8fe2ec56299c30aa43279a
David Novaković <daki.nole@gmail.com>
2026-05-11 11:39:53 +0200

    Added non temlate Result class

---
+++ Server/Server/Common/Result.cs
@@ -0,0 +1,26 @@
+﻿namespace Server.Common
+{
+    public class Result
+    {
+        public bool IsSucces { get; }
+        public string? Error { get; }
+        public ErrorType? ErrorType { get; }
+
+        protected Result(bool isSucces, string? error, ErrorType? errorType)
+        {
+            IsSucces = isSucces;
+            Error = error;
+            ErrorType = errorType;
+        }
+
+        public static Result Success()
+        {
+            return new Result(true, null, null);
+        }
+
+        public static Result Failure(string error, ErrorType errorType)
+        {
+            return new Result(false, error, errorType);
+        }
+    }
+}
