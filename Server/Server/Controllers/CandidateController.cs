ed810a75a83b23655bcb1ab26f7ca75dc941dfc2
David Novaković <daki.nole@gmail.com>
2026-05-11 11:49:27 +0200

    Added Result pattern handling for better api respons

--- Server/Server/Controllers/CandidateController.cs
+++ Server/Server/Controllers/CandidateController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Server.Common;
 using Server.Domain.DTOs;
 using Server.Domain.Models;
 using Server.Services.CandidateService;
@@ -24,49 +25,101 @@ namespace Server.Controllers
         [HttpPost]
         public async Task<ActionResult<Candidate>> AddCandidate(CreateCandidateDto dto)
         {
-            // TODO: improve with Result pattern return type and error handling
             var result = await _candidateService.AddCandidateAsync(dto);
-            if(result == null)
+            if(result.IsSucces)
             {
-                return BadRequest(new { message = "Couldn't create candidate" });
+                return Ok(result.Value);
             }
-            return Ok(result);
+            else
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.Validation => BadRequest(new { message = result.Error }),
+                    ErrorType.NotFound => NotFound(new { message = result.Error }),
+                    _ => StatusCode(500, new { message = result.Error })
+                };
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> RemoveCandidate(int id)
         {
-            // TODO: improve with Result pattern return type and error handling
             var result = await _candidateService.RemoveCandidateAsync(id);
-            return result ? Ok(new { message = "Candidate removed successfully" }) : BadRequest(new { message = "Couldn't remove candidate" });
+            if(result.IsSucces)
+            {
+                return Ok();
+            }
+            else
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.Validation => BadRequest(new { message = result.Error }),
+                    ErrorType.NotFound => NotFound(new { message = result.Error }),
+                    _ => StatusCode(500, new { message = result.Error })
+                };
+            }
         }
 
         [HttpPost("{candidateId}/skills/{skillId}")]
         public async Task<ActionResult> AddSkillToCandidate(int candidateId,int skillId)
         {
-            // TODO: improve with Result pattern return type and error handling
             var result = await _candidateService.AddSkillToCandidateAsync(candidateId, skillId);
-            return Ok(result);
+            if (result.IsSucces)
+            {
+                return Ok();
+            }
+            else
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.Validation => BadRequest(new { message = result.Error }),
+                    ErrorType.NotFound => NotFound(new { message = result.Error }),
+                    _ => StatusCode(500, new { message = result.Error })
+                };
+            }
         }
 
         [HttpDelete("{candidateId}/skills/{skillId}")]
         public async Task<ActionResult> RemoveSkillFromCandidate(int candidateId, int skillId)
         {
-            // TODO: improve with Result pattern return type and error handling
             var result = await _candidateService.RemoveSkillFromCandidateAsync(candidateId, skillId);
-            return result ? Ok(new { message = "Skill removed from candidate successfully" }) : BadRequest(new { message = "Couldn't remove skill from candidate" });
+            if (result.IsSucces)
+            {
+                return Ok();
+            }
+            else
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.Validation => BadRequest(new { message = result.Error }),
+                    ErrorType.NotFound => NotFound(new { message = result.Error }),
+                    _ => StatusCode(500, new { message = result.Error })
+                };
+            }
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Candidate>>> Search([FromQuery] string? name,[FromQuery] List<int>? skillIds)
         {
-            // TODO: improve with Result pattern return type and error handling
             if (string.IsNullOrWhiteSpace(name) && (skillIds == null || skillIds.Count == 0))
             {
                 return BadRequest(new { message = "At least one of the parameters must be provided" });
             }
             var result = await _candidateService.GetCandidateAsync(name, skillIds);
-            return Ok(result);        
+            if (result.IsSucces)
+            {
+                return Ok(result.Value);
+            }
+            else
+            {
+                return result.ErrorType switch
+                {
+                    ErrorType.Validation => BadRequest(new { message = result.Error }),
+                    ErrorType.NotFound => NotFound(new { message = result.Error }),
+                    _ => StatusCode(500, new { message = result.Error })
+                };
+            }
+            ;        
         }
     }
 }
